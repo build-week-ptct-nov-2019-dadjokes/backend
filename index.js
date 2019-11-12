cb7ae8e13942419c779f5d50bcdb5fb32666d3ab
Donald Whitely <dswhitely1@gmail.com>
2019-11-12 13:32:40 -0500

    Main Entry Point to Server

---
+++ index.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const server = require('./api/server');
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Server listening on PORT:${port}`));
