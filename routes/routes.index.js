2cf7349c6fc9f2930a068cfcbdb7ccfdcdc09790
Donald Whitely <dswhitely1@gmail.com>
2019-11-17 20:35:48 -0500

    User Routes Finished

--- routes/routes.index.js
+++ routes/routes.index.js
@@ -1,7 +1,9 @@
 const testRoutes = require('./test/test.routes');
 const authRoutes = require('./auth/auth.routes');
+const userRoutes = require('./user/user.routes');
 
 module.exports = server => {
   server.use('/', testRoutes);
   server.use('/api/auth', authRoutes);
+  server.use('/api/user', userRoutes);
 };
---
+++ routes/user/user.routes.js
@@ -0,0 +1,25 @@
+const userRouter = require('express').Router();
+const Users = require('../../models/auth/auth.model');
+const restricted = require('../../utils/authenticate');
+
+async function updateUser(req, res) {
+  try {
+    const response = await Users.updateUser(req.user.id, req.body);
+    res.json(response);
+  } catch (err) {
+    res.status(500).json();
+  }
+}
+
+async function deleteUser(req, res) {
+  try {
+    await Users.removeUser(req.user.id);
+    res.json(req.user.id);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+userRouter.put('/', restricted, updateUser).delete('/', restricted, deleteUser);
+
+module.exports = userRouter;
